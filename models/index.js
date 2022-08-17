f0b58eea70c2ccea1b6974321160a91a6fe8c8db
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-08-17 11:27:11 +0700

    change database config

--- models/index.js
+++ models/index.js
@@ -1,24 +1,27 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "production";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 console.log(config);
 
-const models = process.cwd() + '/models/' || __dirname;
+const models = process.cwd() + "/models/" || __dirname;
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  console.log(config);
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+	console.log(config);
+	sequelize = new Sequelize(config.database, config.username, config.password, {
+		...config,
+		dialectModule: require("mysql2"),
+	});
 }
 
 // fs
@@ -32,51 +35,51 @@ if (config.use_env_variable) {
 //   db[model.name] = model;
 // });
 
-const files = require.context('./', false, /\.js$/i);
-files.keys().forEach(key => {
-  if(key.includes('index')){
-    return
-  }
-  const model = files(key)(sequelize, Sequelize.DataTypes)
-  db[model.name] = model;
+const files = require.context("./", false, /\.js$/i);
+files.keys().forEach((key) => {
+	if (key.includes("index")) {
+		return;
+	}
+	const model = files(key)(sequelize, Sequelize.DataTypes);
+	db[model.name] = model;
 });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach((modelName) => {
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.Category.belongsTo(db.Asset, {
-  foreignKey: "asset_id",
-  sourceKey: "id"
-})
+	foreignKey: "asset_id",
+	sourceKey: "id",
+});
 db.Asset.hasOne(db.Category, {
-  foreignKey: "asset_id",
-  sourceKey: "id"
-})
+	foreignKey: "asset_id",
+	sourceKey: "id",
+});
 
 db.Product.belongsToMany(db.Asset, {
-  through: db.Product_Asset,
-  foreignKey: "product_id",
-  otherKey: "asset_id"
-})
+	through: db.Product_Asset,
+	foreignKey: "product_id",
+	otherKey: "asset_id",
+});
 db.Asset.belongsToMany(db.Product, {
-  through: db.Product_Asset,
-  foreignKey: "asset_id",
-  otherKey: "product_id"
-})
+	through: db.Product_Asset,
+	foreignKey: "asset_id",
+	otherKey: "product_id",
+});
 
 db.Product.belongsToMany(db.Category, {
-  through: db.Category_Product,
-  foreignKey: "category_id",
-  otherKey: "product_id"
-})
+	through: db.Category_Product,
+	foreignKey: "category_id",
+	otherKey: "product_id",
+});
 db.Category.belongsToMany(db.Product, {
-  through: db.Category_Product,
-  foreignKey: "product_id",
-  otherKey: "category_id"
-})
+	through: db.Category_Product,
+	foreignKey: "product_id",
+	otherKey: "category_id",
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
